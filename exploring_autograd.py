55175427e6b189326924c9d62335c309ae86be8d
Pogeunhan_WJ <significant.butter@gmail.com>
2023-01-02 08:40:30 +0900

    added exploring_gradients and video+3+.ipynb

--- exploring_autograd.py
+++ exploring_autograd.py
@@ -1,3 +1,7 @@
+
+# References
+# https://stackoverflow.com/questions/54754153/autograd-grad-for-tensor-in-pytorch
+
 #%%
 # What does it mean to take a gradient?
 # Let's explore torch.autograd.grad
@@ -10,11 +14,13 @@ gt = torch.ones_like(x)*16 - 0.5 # ground-truths
 print('ground truth', gt)
 
 # Do some computations
-v = x + 2
-y = v ** 2
+y = (x+2) ** 2
 print('y', y)
-dydx = torch.autograd.functional.jacobian(func=y, inputs=x)
-print('dydx', dydx)
+
+dydx = torch.autograd.functional.jacobian(func=lambda var: (var+2)**2, inputs=x)
+print('dydx', dydx.shape)
+print(dydx)
+
 # Compute loss
 loss = torch.nn.MSELoss()(y, gt)
 print('Loss', loss)
@@ -22,3 +28,4 @@ print('Loss', loss)
 # Now compute gradients
 dloss_dx = torch.autograd.grad(outputs=loss, inputs=x)
 print('dloss/dx:', dloss_dx)
+# %%
